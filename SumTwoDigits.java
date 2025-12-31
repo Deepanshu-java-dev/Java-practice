8461da791ffb619400e81d56028b48dd58874630
Deepanshu <deepanshu.bhadoria119677@gmail.com>
2025-12-31 20:51:39 +0530

    Added sum of two digits program

---
+++ SumTwoDigits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Practice3
+{
+	public static void  main(String[] args)
+	{
+		int l,b;
+	Scanner input=new Scanner(System.in);
+	System.out.println("Enter value of l:");
+		l=input.nextInt();		
+	System.out.println("Enter value of b:");
+		b=input.nextInt();
+		int sum=0;
+		 sum= l+b;
+	System.out.println("Sum of two digits :"+sum);	
+	}
+}
