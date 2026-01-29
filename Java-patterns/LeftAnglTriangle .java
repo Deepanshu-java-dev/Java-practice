75adeb3a8572121311ba7bd90b0923c3ef3b47dc
Deepanshu <deepanshu.bhadoria119677@gmail.com>
2026-01-29 20:30:21 +0530

    Create LeftAnglTriangle .java

---
+++ Java-patterns/LeftAnglTriangle .java
@@ -0,0 +1,14 @@
+public class LeftAnglTriangle 
+{
+    public static void main(String[] aegs)
+    {
+        for(int i=0;i<=5;i++)
+        {
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
